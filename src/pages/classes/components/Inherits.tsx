e60374ab86ed23ec4a37284e082ba99b982bee80
mazatf2 <2814748+mazatf2@users.noreply.github.com>
2023-06-01 15:13:51 +0300

    feat: reverse and add padding to inherit/extends table to fix content jumping
    feat: show inherited by count on inherit/extends table
    
    style: switch to bulma

--- src/pages/classes/components/Inherits.tsx
+++ src/pages/classes/components/Inherits.tsx
@@ -1,24 +1,52 @@
 import React, {Fragment} from 'react'
 import {get} from '../../../data/inheritance.mjs'
+import inheritanceJSON from '../../../data/inheritance.json'
 
 export const Inherits = ({className}: { className: string }) => {
-	const inherits = get(className)
+	const inherits = get(className).reverse()
+	const isBaseClass = inheritanceJSON[className]?.inherits?.length == 0
+	if (inherits.length < 4) {
+		let len = 4 - inherits.length
+		for (let i = 0; i < len; i += 1) {
+			inherits.push(['', ['']])
+		}
+	}
+	const InheritedByDirect = ({str}) => {
+		const count = inheritanceJSON[str]?.inheritedBy?.length || ''
+		if (!count) return <>{count}</>
+		return <>
+			{' '}({count})
+		</>
+	}
+	const Label = ({str}) => {
+		if (str === 'PhysicsServer3DRenderingServerHandler') {
+			return <>PhysicsServer3DRendering<br/>ServerHandler</>
+		}
+		return <>{str}</>
+	}
 	return <React.Fragment>
-		<table>
-			<tr>
-				{inherits.map(([parentName]) => <td key={parentName}>
-					{parentName === className ? <b>{parentName}</b> : <a href={'/classes/' + parentName}>{parentName}</a>}
-				</td>)}
-			</tr>
-			<tr>
-				{inherits.map(([parentName, inheritsArr]) => <Fragment key={parentName}>
-					<td>
-						{inheritsArr.map(className => <li>
-							<a href={'/classes/' + className}>{className}</a>
-						</li>)}
-					</td>
-				</Fragment>)}
-			</tr>
-		</table>
+		<div className="columns" style={{whiteSpace: 'nowrap'}}>
+			{inherits.map(([parentName, inheritsArr]) => <div className='column'>
+				{parentName === className ? <b>{parentName}</b> : <a href={'/classes/' + parentName}>{parentName}</a>}<br/>
+				{inheritsArr[0] && <hr/>}
+				{inheritsArr.map(className => <>
+					{className && <>
+						{'- '}
+						<a href={'/classes/' + className}>
+							<span><Label str={className}/></span>
+						</a>
+						<span><InheritedByDirect str={className}/></span>
+						<br/>
+					</>}
+				</>)}
+			</div>
+			)}
+		</div>
+		{isBaseClass && <Fragment>
+			<hr style={{
+				width: '30rem',
+				height: '0.7rem',
+			}}/>
+		</Fragment>}
 	</React.Fragment>
 }
